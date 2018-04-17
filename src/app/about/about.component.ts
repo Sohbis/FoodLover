b0962ab21cf98c86ed8fde44d8daaea13d147e17
Sohbis <Soham.biswas.19@gmail.com>
2018-04-17 18:58:45 +0530

    First

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  leader: Leader[];
+  constructor(private leaderService: LeaderService ) { }
+
+  ngOnInit() {
+    // this.leader = this.leaderService.getLeaders();
+    this.leaderService.getLeaders().
+    subscribe(leader => this.leader = leader);
+
+
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {  FormsModule  } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { MdDialog, MdDialogRef } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import 'hammerjs';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { baseURL } from './shared/baseurl';
+
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService} from './services/leader.service';
+import { LoginComponent } from './login/login.component';
+import { ProcessHttpmsgService } from './services/process-httpmsg.service';
+
+import { enterView } from '@angular/core/src/render3/instructions';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
+  ],
+  entryComponents: [
+    LoginComponent
+  ],
+  providers: [DishService, PromotionService, LeaderService, ProcessHttpmsgService, { provide: 'BaseURL', useValue: baseURL } ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
